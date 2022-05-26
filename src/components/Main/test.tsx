f8b88227f1c2093ad77955155274abe2a0e60474
douglasneves-net <d88.neves@gmail.com>
2022-05-25 22:46:27 -0300

    feat: Add Snapshot
    Add Snapshot

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -4,9 +4,11 @@ import Main from '.'
 
 describe('<Main />', () => {
   it('should render the heading', () => {
-    render(<Main />)
+    const { container } = render(<Main />)
     expect(
       screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
+    //Cria um snapshot na primeira vez, depois compara, então se mudar o conteudo ele vai perceber a mudança e notificar.
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
